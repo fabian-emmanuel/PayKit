d44066d2a97229c939f46cc7d321d1a7d6afa6d4
Fabian Emmanuel <emmanuel.fabian.dev@gmail.com>
2022-06-07 12:52:47 +0100

    In The Beginning Commit

---
+++ src/main/java/com/codewithfibbee/paykit/PayKitApplication.java
@@ -0,0 +1,13 @@
+package com.codewithfibbee.paykit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayKitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayKitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codewithfibbee/paykit/PayKitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewithfibbee.paykit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayKitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

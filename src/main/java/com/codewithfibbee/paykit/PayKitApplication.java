086fbe34ae8ce596bcc78a0c3d360026adde0568
Fabian Emmanuel <emmanuel.fabian.dev@gmail.com>
2022-06-07 22:48:58 +0100

    add properties configuration files

--- src/main/java/com/codewithfibbee/paykit/PayKitApplication.java
+++ src/main/java/com/codewithfibbee/paykit/PayKitApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PayKitApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PayKitApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PayKitApplication.class, args);
+    }
 
 }
